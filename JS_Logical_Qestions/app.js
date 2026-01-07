382e4c3a630e564741846a42ebd7ed7ef7757f10
Akshay Mishra <145044850+theakshaymishra@users.noreply.github.com>
2026-01-07 08:51:38 +0000

    Code added to app.js file!

--- JS_Logical_Qestions/app.js
+++ JS_Logical_Qestions/app.js
@@ -1,1 +1,32 @@
 console.log("Akshay")
+let startNum = 2;
+let endNum = prompt("Enter that number up to which you want to print even number! ")
+endNum = Number(endNum)
+if(endNum && endNum>1 && !isNaN(endNum)){
+    while(startNum <= endNum){
+    console.log(startNum);
+    startNum += 2;
+    }
+} else{
+    console.log("Enter digit greater than 1")
+}
+
+// let startNum = 2;
+// let endNum = prompt("Enter that number up to which you want to print even number! ")
+// endNum = Number(endNum)
+
+// if(endNum && endNum > 1 && !isNaN(endNum)){
+    
+//     // Yahan humne check kiya: Kya number odd hai?
+//     // Agar 11 % 2 karenge toh 1 bachega (True)
+//     if(endNum % 2 !== 0){
+//         endNum = endNum - 1; // 11 ko 10 bana diya
+//     }
+
+//     while(startNum <= endNum){
+//         console.log(endNum);
+//         endNum -= 2;
+//     }
+// } else {
+//     console.log("Enter digit greater than 1")
+}
